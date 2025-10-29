dc98addd6922f869deb5a370cf65e8c607ad1dfa
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-10-29 09:22:06 +0300

    Add complex queries using different types of joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,17 @@
+-- Retrieves all bookings and the respective users who made those bookings.
+SELECT *
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+-- Retrieves all properties and their reviews, including properties that have no reviews.
+
+SELECT *
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
+
+-- Retrieves all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+SELECT *
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
